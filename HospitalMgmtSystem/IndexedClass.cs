4b3e7c326608fba2d9a14bba40ae4996383bd76e
prebiton <48591939+prebiton@users.noreply.github.com>
2021-09-14 17:05:48 +0530

    Add files via upload

---
+++ HospitalMgmtSystem/IndexedClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgmtSystem
+{
+    interface IUser
+    {
+        int a { get; set; }
+        void work();
+    }
+
+    class User: IUser
+    {
+        public int a { get => a; set => a = value; }
+        public void work()
+        {
+
+        }
+    }
+    class IndexedClass
+    {
+        private string[] names = new string[10];
+
+        const int a = 10; // must have value initially
+        readonly int b; // can assign value in constructor
+
+        IndexedClass()
+        {
+            b = 20;
+        }
+        public string this[int index]
+        {
+            get
+            {
+                return (index >= 0 && index <= 9) ? names[index] : "Invalid";
+            }
+            set
+            {
+                if (index >= 0 && index <= 9)
+                {
+                    names[index] = value;
+                }
+            }
+        }
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+                while(index < 10)
+                {
+                    if(names[index] == name)
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+                return -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            IndexedClass nameList = new IndexedClass();
+            nameList[0] = "John";
+            nameList[1] = "Smith";
+            Console.WriteLine(nameList[0]);
+            Console.WriteLine(nameList[10]);
+            Console.WriteLine(nameList["Smithh"]);
+        }
+    }
+}
---
+++ HospitalMgmtSystem/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgmtSystem
+{
+    class Person
+    {
+        public string title;
+        public string firstName;
+        public string lastName;
+        public string date;
+        public string gender;
+        public string address;
+        public string phone;
+        public string email;
+    }
+
+    class Staff: Person
+    {
+        public string staffId;
+        public string joined;
+        public string[] education;
+        public string[] certification;
+        public string[] languagesKnown;
+    }
+
+    class Doctor: Staff
+    {
+        public string[] speciality;
+        void prescribeMedication() { }
+        void diagnose() { }
+        void reccomendProcedure() { }
+    }
+
+    class Nurse: Staff
+    {
+        void testVitals() { }
+    }
+
+    class Admin: Staff
+    {
+        void listUsers() { }
+        void disableUser() { }
+    }
+
+    class Patient: Person
+    {
+        public string patientId;
+        public string race;
+        public string ethinicity;
+        public string[] languagesKnown;
+        public string[] nomineeIds;
+        public string[] prescriptions;
+        public struct allergies
+        {
+            public string[] type;
+            public bool[] isFatal;
+        }
+
+        void editDetails() { }
+        void scheduleAppointment() { }
+        void checkVisits() { }
+    }
+
+    class Nominee: Person
+    {
+        public string nomineeId;
+        public string patientId;
+        public string relationship;
+    }
+
+    class Visit
+    {
+        public string visitId;
+        public string appoinmentId;
+        public string height;
+        public string weight;
+        public string[] bloodPressure;
+        public string bodyTemp;
+        public string respirationRate;
+        public struct descript
+        {
+            public string[] main;
+            public string[] description;
+        }
+        public descript diagnosis;
+        public descript procedure;
+        public descript medication;
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
